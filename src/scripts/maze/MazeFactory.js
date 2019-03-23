2fa3ff894c357e9f479f5e6d6f72d955a4da7ae4
W01fw00d <gabrm_91@hotmail.com>
2019-03-23 17:24:48 +0100

    Avoid global variables, refactor into class atributes and some methods

--- src/scripts/maze/MazeFactory.js
+++ src/scripts/maze/MazeFactory.js
@@ -1,77 +1,39 @@
-var width = 25           //Number paths fitted horisontally
-var height = 25          //Number paths fitted vertically
-var x = width/2|0        //Horisontal starting position
-var y = height/2|0       //Vertical starting position
-var route;
-var map;
-var random;
-var direction;
-var ctx;
-var offset;
-var timer;
-
 class MazeFactory {
   constructor() {
+    this.width = 25;
+    this.x = this.width / 2 | 0        //Horisontal starting position
+    this.height = 25          //Number paths fitted vertically
+    this.y = this.height / 2 | 0       //Vertical starting position
+
+    this.route = [[this.x, this.y]];
+    this.map = [];
+    const seed = Math.random() * 100000 | 0//Seed for random numbers
+    console.log(seed);
+    this.random = this.randomGen(seed);
+
     const pathWidth = 10       //Width of the Maze Path
     const wall = 2             //Width of the Walls between Paths
     const outerWall = 2        //Width of the Outer most wall
 
-    const delay = 1            //Delay between algorithm cycles
+    this.makeMap(
+      this.width,
+      this.x,
+      this.height,
+      this.y,
+      this.map
+    );
 
-    const seed = Math.random()*100000|0//Seed for random numbers
-    console.log(seed);
-    const wallColor = '	#a02040'   //Color of the walls
-    const pathColor = '#f5e8eb'//Color of the path
-    const cursorColor = '#d9a5b2' //TODO not used yet
+    const canvas = this.makeCanvas(outerWall, this.width, this.height, pathWidth, wall);
 
-    const randomGen = function(seed){
-    	if(seed===undefined)var seed=performance.now()
-    	return function(){
-        seed = (seed * 9301 + 49297) % 233280
-    		return seed/233280
-    	}
-    }
+    this.offset = pathWidth / 2 + outerWall;
 
-    init = function(){
-      offset = pathWidth/2+outerWall
-      map = []
-      const canvas = document.querySelector('canvas')
-      ctx = canvas.getContext('2d')
-      canvas.width = outerWall*2+width*(pathWidth+wall)-wall
-      canvas.height = outerWall*2+height*(pathWidth+wall)-wall
-      ctx.fillStyle = wallColor
-      ctx.fillRect(0,0,canvas.width,canvas.height)
-      random = randomGen(seed)
-      ctx.strokeStyle = pathColor
-      ctx.lineCap = 'square'
-      ctx.lineWidth = pathWidth
-      ctx.beginPath()
-      for(var i=0;i<height*2;i++){
-        map[i] = []
-        for(var j=0;j<width*2;j++){
-          map[i][j] = false
-        }
-      }
-      map[y*2][x*2] = true
-      route = [[x,y]]
-      ctx.moveTo(x*(pathWidth+wall)+offset,
-                 y*(pathWidth+wall)+offset)
-    }
-    init()
+    this.ctx = this.configCtx(canvas, pathWidth, wall, this.offset, this.x, this.y);
 
-    const inputWidth = document.getElementById('width')
-    const inputHeight = document.getElementById('height')
-    const inputPathWidth = document.getElementById('pathwidth')
-    const inputWallWidth = document.getElementById('wallwidth')
-    const inputOuterWidth = document.getElementById('outerwidth')
-    const inputPathColor = document.getElementById('pathcolor')
-    const inputWallColor = document.getElementById('wallcolor')
-    const inputSeed = document.getElementById('seed')
-    const buttonRandomSeed = document.getElementById('randomseed')
-
-    const loop = function(){
-      x = route[route.length-1][0]|0
-      y = route[route.length-1][1]|0
+    const loop = function(route, map, random, ctx, offset){
+      const delay = 1; //Delay between algorithm cycles
+      let direction;
+      const x = route[route.length-1][0]|0
+      const y = route[route.length-1][1]|0
 
       var directions = [[1,0],[-1,0],[0,1],[0,-1]],
           alternatives = []
@@ -88,7 +50,16 @@ class MazeFactory {
         if(route.length>0){
           ctx.moveTo(route[route.length-1][0]*(pathWidth+wall)+offset,
                      route[route.length-1][1]*(pathWidth+wall)+offset)
-          timer = setTimeout(loop,delay)
+
+          setTimeout(() => {
+            loop(
+              route,
+              map,
+              random,
+              ctx,
+              offset
+            );
+          }, delay);
         }
         return;
       }
@@ -99,9 +70,73 @@ class MazeFactory {
       map[(direction[1]+y)*2][(direction[0]+x)*2] = true
       map[direction[1]+y*2][direction[0]+x*2] = true
       ctx.stroke()
-      timer = setTimeout(loop,delay)
+
+      setTimeout(() => {
+        loop(
+          route,
+          map,
+          random,
+          ctx,
+          offset
+        )
+      }, delay);
     }
-    loop()
+
+    loop(
+      this.route,
+      this.map,
+      this.random,
+      this.ctx,
+      this.offset
+    )
+  }
+
+  randomGen(seed) {
+    if (seed === undefined) {
+      let seed = performance.now();
+    }
+    return () => {
+      seed = (seed * 9301 + 49297) % 233280;
+      return seed / 233280;
+    }
+  }
+
+  makeMap(width, x, height, y, map){
+    for(var i=0;i<height*2;i++){
+      map[i] = []
+      for(var j=0;j<width*2;j++){
+        map[i][j] = false
+      }
+    }
+    map[y*2][x*2] = true
+  }
+
+  makeCanvas(outerWall, width, height, pathWidth, wall) {
+    const canvas = document.querySelector('canvas')
+
+    canvas.width = outerWall*2+width*(pathWidth+wall)-wall
+    canvas.height = outerWall*2+height*(pathWidth+wall)-wall
+
+    return canvas;
+  }
+
+  configCtx(canvas, pathWidth, wall, offset, x, y) {
+    const wallColor = '	#a02040'   //Color of the walls
+    const pathColor = '#f5e8eb'//Color of the path
+    const cursorColor = '#d9a5b2' //TODO not used yet
+
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = wallColor;
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.strokeStyle = pathColor;
+    ctx.lineCap = 'square';
+    ctx.lineWidth = pathWidth;
+    ctx.beginPath();
+
+    ctx.moveTo(x*(pathWidth+wall)+offset,
+               y*(pathWidth+wall)+offset);
+
+    return ctx;
   }
 
 }
