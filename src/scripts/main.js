08c48b0c09d8b680d06a4d6dc6d1169609e2f2f2
W01fw00d <gabrm_91@hotmail.com>
2019-04-19 12:41:17 +0200

    Improve start button style

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,9 @@ function init() {
 function playAudio() {
   const brainSong = document.getElementById("brain_song");
 
+  const audioButton = document.getElementsByClassName("audioButton")[0];
+  audioButton.className += " pressed";
+
   brainSong.play();
 
   setTimeout(() => {
